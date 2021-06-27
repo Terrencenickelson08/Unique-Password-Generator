74ffa0a025a9fd58ed429e75ffe9e7234205b600
John Doe <54046494+Terrencenickelson08@users.noreply.github.com>
2021-06-27 03:06:53 -0400

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,57 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'pw';
+  length = 0;
+  changedLetter = false;
+  changedNumber = false;
+  changedSymbol = false;
+  password = '';
+
+  onChangeLetter(){
+    this.changedLetter = true;
+  }
+
+  onChangeNumber(){
+    this.changedNumber = true;
+  }
+
+  onChangeSymbol(){
+    this.changedSymbol = true;
+  }
+
+  onButtonClick(){
+    const numbers = '1234567879';
+    const letters = 'abcdefghijklmnopqrstuvwxyz';
+    const symbols = '!@#$%^&*';
+
+    let validChars = '';
+    if(this.changedLetter){
+      validChars += letters;
+    }
+
+    if(this.changedNumber){
+      validChars += numbers;
+    }
+
+    if(this.changedSymbol){
+      validChars += symbols;
+    }
+
+    let generatedPassword= '';
+    for(let i = 0; i < this.length; i++){
+      const index = Math.floor(Math.random() * validChars.length);
+      generatedPassword += validChars[index];
+    }
+    this.password = generatedPassword;
+
+  }
+
+  onChangeLength(event: Event){
+    const target = event.target as HTMLInputElement;
+    const parsedValue = parseInt(target.value);
+
+    if(!isNaN(parsedValue)){
+      this.length = parsedValue;
+    }
+  }
 }
